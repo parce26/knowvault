a7b90e8c7b12b788dc0a57f886cf47618fd49dfd
parce26 <gonzaleztabaressebastian@gmail.com>
2026-02-25 22:53:25 -0600

    Fix User model - add password field

--- knowvault/src/main/java/com/knowvault/model/User.java
+++ knowvault/src/main/java/com/knowvault/model/User.java
@@ -5,18 +5,23 @@ public class User {
     private Long id;
     private String name;
     private String email;
+    private String password;
     private String role;
 
+    // Constructor vacío
     public User() {
-        // constructor vacío
     }
 
-    public User(Long id, String name, String email, String role) {
+    // Constructor completo
+    public User(Long id, String name, String email, String password, String role) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.password = password;
         this.role = role;
     }
+
+    // Getters y Setters
 
     public Long getId() {
         return id;
@@ -40,6 +45,14 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getRole() {
